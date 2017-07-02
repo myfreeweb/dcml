2b23ea8f9f91f8587f3f1760a2fb52bf7fa7be61
Greg V <greg@unrelenting.technology>
2017-07-03 01:08:23 +0300

    fmt

--- src/de.rs
+++ src/de.rs
@@ -82,7 +82,14 @@ impl<'de> de::Deserializer<'de> for Deserializer {
         where V: Visitor<'de>
     {
         match self.input {
-            Number(ref x) => visitor.visit_i8((*x).to_integer().to_i8().ok_or(VError::invalid_value(Deserializer::unexpected(Number((*x).clone())), &"a number that fits into i8"))?),
+            Number(ref x) => {
+                visitor.visit_i8(
+                    (*x).to_integer().to_i8().ok_or(VError::invalid_value(
+                        Deserializer::unexpected(Number((*x).clone())),
+                        &"a number that fits into i8",
+                    ))?,
+                )
+            },
             x => Err(VError::invalid_type(Deserializer::unexpected(x), &"a number")),
         }
     }
@@ -91,7 +98,14 @@ impl<'de> de::Deserializer<'de> for Deserializer {
         where V: Visitor<'de>
     {
         match self.input {
-            Number(ref x) => visitor.visit_i16((*x).to_integer().to_i16().ok_or(VError::invalid_value(Deserializer::unexpected(Number((*x).clone())), &"a number that fits into i16"))?),
+            Number(ref x) => {
+                visitor.visit_i16(
+                    (*x).to_integer().to_i16().ok_or(VError::invalid_value(
+                        Deserializer::unexpected(Number((*x).clone())),
+                        &"a number that fits into i16",
+                    ))?,
+                )
+            },
             x => Err(VError::invalid_type(Deserializer::unexpected(x), &"a number")),
         }
     }
@@ -100,7 +114,14 @@ impl<'de> de::Deserializer<'de> for Deserializer {
         where V: Visitor<'de>
     {
         match self.input {
-            Number(ref x) => visitor.visit_i32((*x).to_integer().to_i32().ok_or(VError::invalid_value(Deserializer::unexpected(Number((*x).clone())), &"a number that fits into i32"))?),
+            Number(ref x) => {
+                visitor.visit_i32(
+                    (*x).to_integer().to_i32().ok_or(VError::invalid_value(
+                        Deserializer::unexpected(Number((*x).clone())),
+                        &"a number that fits into i32",
+                    ))?,
+                )
+            },
             x => Err(VError::invalid_type(Deserializer::unexpected(x), &"a number")),
         }
     }
@@ -109,7 +130,14 @@ impl<'de> de::Deserializer<'de> for Deserializer {
         where V: Visitor<'de>
     {
         match self.input {
-            Number(ref x) => visitor.visit_i64((*x).to_integer().to_i64().ok_or(VError::invalid_value(Deserializer::unexpected(Number((*x).clone())), &"a number that fits into i64"))?),
+            Number(ref x) => {
+                visitor.visit_i64(
+                    (*x).to_integer().to_i64().ok_or(VError::invalid_value(
+                        Deserializer::unexpected(Number((*x).clone())),
+                        &"a number that fits into i64",
+                    ))?,
+                )
+            },
             x => Err(VError::invalid_type(Deserializer::unexpected(x), &"a number")),
         }
     }
@@ -118,7 +146,14 @@ impl<'de> de::Deserializer<'de> for Deserializer {
         where V: Visitor<'de>
     {
         match self.input {
-            Number(ref x) => visitor.visit_u8((*x).to_integer().to_u8().ok_or(VError::invalid_value(Deserializer::unexpected(Number((*x).clone())), &"a number that fits into u8"))?),
+            Number(ref x) => {
+                visitor.visit_u8(
+                    (*x).to_integer().to_u8().ok_or(VError::invalid_value(
+                        Deserializer::unexpected(Number((*x).clone())),
+                        &"a number that fits into u8",
+                    ))?,
+                )
+            },
             x => Err(VError::invalid_type(Deserializer::unexpected(x), &"a number")),
         }
     }
@@ -127,7 +162,14 @@ impl<'de> de::Deserializer<'de> for Deserializer {
         where V: Visitor<'de>
     {
         match self.input {
-            Number(ref x) => visitor.visit_u16((*x).to_integer().to_u16().ok_or(VError::invalid_value(Deserializer::unexpected(Number((*x).clone())), &"a number that fits into u16"))?),
+            Number(ref x) => {
+                visitor.visit_u16(
+                    (*x).to_integer().to_u16().ok_or(VError::invalid_value(
+                        Deserializer::unexpected(Number((*x).clone())),
+                        &"a number that fits into u16",
+                    ))?,
+                )
+            },
             x => Err(VError::invalid_type(Deserializer::unexpected(x), &"a number")),
         }
     }
@@ -136,7 +178,14 @@ impl<'de> de::Deserializer<'de> for Deserializer {
         where V: Visitor<'de>
     {
         match self.input {
-            Number(ref x) => visitor.visit_u32((*x).to_integer().to_u32().ok_or(VError::invalid_value(Deserializer::unexpected(Number((*x).clone())), &"a number that fits into u32"))?),
+            Number(ref x) => {
+                visitor.visit_u32(
+                    (*x).to_integer().to_u32().ok_or(VError::invalid_value(
+                        Deserializer::unexpected(Number((*x).clone())),
+                        &"a number that fits into u32",
+                    ))?,
+                )
+            },
             x => Err(VError::invalid_type(Deserializer::unexpected(x), &"a number")),
         }
     }
@@ -145,7 +194,14 @@ impl<'de> de::Deserializer<'de> for Deserializer {
         where V: Visitor<'de>
     {
         match self.input {
-            Number(ref x) => visitor.visit_u64((*x).to_integer().to_u64().ok_or(VError::invalid_value(Deserializer::unexpected(Number((*x).clone())), &"a number that fits into u64"))?),
+            Number(ref x) => {
+                visitor.visit_u64(
+                    (*x).to_integer().to_u64().ok_or(VError::invalid_value(
+                        Deserializer::unexpected(Number((*x).clone())),
+                        &"a number that fits into u64",
+                    ))?,
+                )
+            },
             x => Err(VError::invalid_type(Deserializer::unexpected(x), &"a number")),
         }
     }
@@ -154,7 +210,18 @@ impl<'de> de::Deserializer<'de> for Deserializer {
         where V: Visitor<'de>
     {
         match self.input {
-            Number(ref x) => visitor.visit_f32((*x).numer().to_f32().ok_or(VError::invalid_value(Deserializer::unexpected(Number((*x).clone())), &"a number that is valid as f32"))? / (*x).denom().to_f32().ok_or(VError::invalid_value(Deserializer::unexpected(Number((*x).clone())), &"a number that is valid as f32"))?),
+            Number(ref x) => {
+                visitor.visit_f32(
+                    (*x).numer().to_f32().ok_or(VError::invalid_value(
+                        Deserializer::unexpected(Number((*x).clone())),
+                        &"a number that is valid as f32",
+                    ))? /
+                    (*x).denom().to_f32().ok_or(VError::invalid_value(
+                        Deserializer::unexpected(Number((*x).clone())),
+                        &"a number that is valid as f32",
+                    ))?,
+                )
+            },
             x => Err(VError::invalid_type(Deserializer::unexpected(x), &"a number")),
         }
     }
@@ -163,7 +230,18 @@ impl<'de> de::Deserializer<'de> for Deserializer {
         where V: Visitor<'de>
     {
         match self.input {
-            Number(ref x) => visitor.visit_f64((*x).numer().to_f64().ok_or(VError::invalid_value(Deserializer::unexpected(Number((*x).clone())), &"a number that is valid as f64"))? / (*x).denom().to_f64().ok_or(VError::invalid_value(Deserializer::unexpected(Number((*x).clone())), &"a number that is valid as f64"))?),
+            Number(ref x) => {
+                visitor.visit_f64(
+                    (*x).numer().to_f64().ok_or(VError::invalid_value(
+                        Deserializer::unexpected(Number((*x).clone())),
+                        &"a number that is valid as f64",
+                    ))? /
+                    (*x).denom().to_f64().ok_or(VError::invalid_value(
+                        Deserializer::unexpected(Number((*x).clone())),
+                        &"a number that is valid as f64",
+                    ))?,
+                )
+            },
             x => Err(VError::invalid_type(Deserializer::unexpected(x), &"a number")),
         }
     }
@@ -189,7 +267,10 @@ mod tests {
     #[test]
     fn test_numbers() {
         assert_eq!(-128, from_value::<i8>(int(b"-128", 10)).unwrap());
-        assert_eq!(Err(VError::custom("invalid value: number, expected a number that fits into i8")), from_value::<i8>(int(b"128", 10)));
+        assert_eq!(
+            Err(VError::custom("invalid value: number, expected a number that fits into i8")),
+            from_value::<i8>(int(b"128", 10))
+        );
         assert_eq!(2.5, from_value::<f32>(rat(b"5", b"2", 10)).unwrap());
     }
 
@@ -200,14 +281,20 @@ mod tests {
 
     #[test]
     fn test_array() {
-        assert_eq!(vec!["hello".to_owned(), "world".to_owned()], from_value::<Vec<String>>(arr(vec![Value::Text("hello".to_owned()), Value::Text("world".to_owned())])).unwrap());
+        assert_eq!(
+            vec!["hello".to_owned(), "world".to_owned()],
+            from_value::<Vec<String>>(arr(vec![Value::Text("hello".to_owned()), Value::Text("world".to_owned())])).unwrap()
+        );
     }
 
     #[test]
     fn test_dict() {
         let mut btm = BTreeMap::new();
         btm.insert("hello".to_owned(), "world".to_owned());
-        assert_eq!(btm, from_value::<BTreeMap<String, String>>(Value::Dict(map!{"hello".to_owned() => Value::Text("world".to_owned())})).unwrap());
+        assert_eq!(
+            btm,
+            from_value::<BTreeMap<String, String>>(Value::Dict(map!{"hello".to_owned() => Value::Text("world".to_owned())})).unwrap()
+        );
     }
 
     #[test]
@@ -222,8 +309,15 @@ mod tests {
             name: String,
             stuff: Box<Y>,
         }
-        assert_eq!(X { name: "test".to_owned(), stuff: Box::new(Y {count: Some(123), x: None}) },
-                   from_value::<X>(Value::Dict(map!{"name".to_owned() => Value::Text("test".to_owned()), "stuff".to_owned() => Value::Dict(map!{ "count".to_owned() => int(b"123", 10) })})).unwrap());
+        assert_eq!(
+            X {
+                name: "test".to_owned(),
+                stuff: Box::new(Y { count: Some(123), x: None }),
+            },
+            from_value::<X>(Value::Dict(
+                    map!{"name".to_owned() => Value::Text("test".to_owned()), "stuff".to_owned() => Value::Dict(map!{ "count".to_owned() => int(b"123", 10) })},
+                )).unwrap()
+        );
         #[derive(Deserialize, Debug, PartialEq)]
         struct U;
         assert_eq!(U, from_value::<U>(Value::Void).unwrap());
@@ -235,11 +329,15 @@ mod tests {
         enum X {
             One(u32),
             Two,
-            Three { f: String }
+            Three { f: String },
         }
         assert_eq!(X::One(0), from_value::<X>(Value::Tagged("One".to_string(), Box::new(int(b"0", 10)))).unwrap());
         assert_eq!(X::Two, from_value::<X>(Value::Tagged("Two".to_string(), Box::new(Value::Void))).unwrap());
-        assert_eq!(X::Three { f: "Hello".to_string() }, from_value::<X>(Value::Tagged("Three".to_string(), Box::new(Value::Dict(map!{ "f".to_owned() => Value::Text("hello".to_owned()) })))).unwrap());
+        assert_eq!(X::Three { f: "Hello".to_string() },
+            from_value::<X>(Value::Tagged(
+                "Three".to_string(),
+                Box::new(Value::Dict(map!{ "f".to_owned() => Value::Text("hello".to_owned()) })),
+            )).unwrap());
     }
 
 }
